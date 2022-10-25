e18c8b8cfc7985797fb89f6313cfa76dc7ce53d6
Seferi <Sefersakir4@gmail.com>
2022-10-25 11:02:54 +0200

    final edition

--- src/App.js
+++ src/App.js
@@ -1,83 +1,82 @@
-import React from 'react';
-import './App.css';
-import Axios from "axios";
-import{useEffect, useState} from "react";
+import React from "react";
+import  Axios  from "axios";
+import { useState } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
 import GOT from './components/GOT.jpg'
+import BookIcon from '@mui/icons-material/Book';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import FaceIcon from '@mui/icons-material/Face';
+import HouseIcon from '@mui/icons-material/House';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'; 
+
+const App=()=> {
+
+    const [books,setBooks]=useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
 
-function App() {
+    const getBooks = () => {
+        setIsLoading(true);
+        Axios.get("https://www.anapioficeandfire.com/api/books?pageSize=30")
+        .then((res)=>{
+            setBooks(res.data)
+            setIsLoading(false);
+        });
+      }
 
-  const [books,setBooks]=useState([]);
+      const handlech = (e) => {
+        const type = e.target.value
+        if(type === "ASC") {
+            setBooks([...books].sort((a,b) => {
+                return b.released - a.released ? 1 : -1;
+            }))
+        } else if(type==="DSC"){
+            setBooks([...books].sort((a,b) => {
+                return a.released - b.released ? 1 : -1;
+            }))
+        }
+      }
 
-  const getBooks = () => {
-    Axios.get("https://www.anapioficeandfire.com/api/books?pageSize=30")
-    .then((res)=>{
-      console.log(res);
-        setBooks(res.data)
-        console.log(res.data[0])
-    });
-  }
-  const [isHovering, setIsHovering] = useState(false);
-
-  const handleMouseOver = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
-
-
-  
-  return (
+      return(
     <div>
       <h1>Game of Thrones Books</h1>
       <h5>Fetch a list from an API and Display it!</h5>
       <div className="button-container-div">
-        <button onClick={getBooks} className="ui secondary basic button">Fetch Data</button>
+        <button onClick={getBooks} disabled={isLoading} className="ui secondary basic button">Fetch Data</button>
+        <select onChange={handlech} className="dropdown">
+            <option value="">Select Sorting Method</option>
+            <option value="ASC" className="ui secondary basic button">New to Old</option>
+            <option value="DSC" className="ui secondary basic button">Old to New</option>
+        </select>
     </div>
-    <div className='flexContainer'>
+      {isLoading ? <LoadingSpinner/> : false}
+    <div className='flexContainer' >
     {
       books &&
       books.map((book ,i)=>{
         return (
-          <div class="flip-card">
-            <div class="flip-card-inner">
-              <div class="flip-card-front">
+          <div className="column" key={i}>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
                 <img src={GOT} alt="Avatar" style={{width:"300px",height:"300px"}} />
               </div>
-              <div class="flip-card-back">
-                <h1>Book {i+1}</h1> 
-                <h2 className="bookname">{book.name}</h2>
-                     <p>{book.authors}</p>
-                     <p>{book.numberOfPages}</p>
-                     <p>{book.country}</p>
-                <p>{book.released}</p>
+              <div className="flip-card-back" key={i}>
+                <h1 >Book {i+1}</h1> 
+                <h2 className="bookname" key={book}>{book.name} &nbsp; <BookIcon/></h2>
+                    <p> {book.authors}  &nbsp;{<FaceIcon/>}</p>
+                    <p>{book.numberOfPages} &nbsp;{<LibraryBooksIcon/>}</p>
+                    <p>{book.country} &nbsp; {<HouseIcon/>}</p>
+                    <p>{new Date(book.released).toDateString()} &nbsp; {<CalendarMonthIcon/>}</p>
               </div>
             </div>
           </div>
-          // <div>
-          //   <div className=''>
-          //     <div className='one-book'>
-          //       <img src={GOT} alt='Image' style={{width: 300,height:300}}></img> 
-          //     </div>
-          //     <div className='bookCards' key={book.name}>
-          //           {[book.id,<h4  key={i}>Book {i+1}</h4>]}
-          //           <h2 className="bookname">{book.name}</h2>
-          //           <p>{book.authors}</p>
-          //           <p>{book.numberOfPages}</p>
-          //           <p>{book.country}</p>
-          //           <p>{book.released}</p>
-          //       </div>
-      
-          //   </div>
-          // </div>
+        </div>       
               )
           })
-        }       
+        } 
     </div>
     </div>
-  );
+      )
 }
-
 export default App;
--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -5,7 +5,7 @@ export default function LoadingSpinner() {
     return(
         <div className="center">
         <div className="spinner-container">
-            <div className="loading-spinner">
+            <div className="loading-spinner" >
             </div>
         </div>
         </div>
